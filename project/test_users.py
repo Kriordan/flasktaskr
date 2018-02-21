ee3ed81f65d4148842d2728fa481eb55f7fdec34
Keith Riordan <keith.riordan@gmail.com>
2018-02-21 09:15:53 -0500

    Add test suite. Add bootstrap styles. Add role to User model

---
+++ project/test_users.py
@@ -0,0 +1,135 @@
+import os
+import unittest
+
+from views import app, db
+from _config import basedir
+from models import User
+
+TEST_DB = 'test.db'
+
+
+class UsersTests(unittest.TestCase):
+
+    # setup and teardown
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+            os.path.join(basedir, TEST_DB)
+        self.app = app.test_client()
+        db.create_all()
+
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+
+    # helper methods
+
+    def login(self, name, password):
+        return self.app.post('/', data=dict(
+            name=name, password=password), follow_redirects=True)
+
+
+    def register(self, name, email, password, confirm):
+        return self.app.post(
+            'register/',
+            data=dict(name=name, email=email, password=password, confirm=confirm),
+            follow_redirects=True
+        )
+
+
+    def logout(self):
+        return self.app.get('logout/', follow_redirects=True)
+
+
+    def create_user(self, name, email, password):
+        new_user = User(name=name, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+
+    # tests
+
+    def test_user_can_register(self):
+        new_user = User("keith", "keith@keith.org", "keithriordan")
+        db.session.add(new_user)
+        db.session.commit()
+        test = db.session.query(User).all()
+        for t in test:
+            t.name
+        assert t.name == "keith"
+
+
+    def test_form_is_present(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Please sign in to access your task list',
+                      response.data)
+
+
+    def test_users_cannot_login_unless_registered(self):
+        response = self.login('foo', 'bar')
+        self.assertIn(b'Invalid username or password.', response.data)
+
+
+    def test_users_can_login(self):
+        self.register('Christopher', 'keith@test.com', 'password', 'password')
+        response = self.login('Christopher', 'password')
+        self.assertIn(b'Welcome!', response.data)
+
+
+    def test_invalid_form_data(self):
+        self.register('Christopher', 'chris@test.com', 'password', 'password')
+        response = self.login('alert("alert box!");', 'foo')
+        self.assertIn(b'Invalid username or password.', response.data)
+
+
+    def test_form_is_present_on_register_page(self):
+        response = self.app.get('register/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Please register to access the task list.',
+                      response.data)
+
+
+    def test_user_registration(self):
+        self.app.get('register/', follow_redirects=True)
+        response = self.register(
+            'Christopher', 'chris@test.com', 'password', 'password')
+        self.assertIn(b'Thanks for registering. Please login.', response.data)
+
+
+    def test_user_registration_error(self):
+        self.app.get('register/', follow_redirects=True)
+        self.register('Christopher', 'chris@test.com', 'password', 'password')
+        self.app.get('register/', follow_redirects=True)
+        response = self.register('Christopher', 'chris@test.com', 'password', 'password')
+        self.assertIn(b'That username and/or email already exists.',
+                      response.data)
+
+
+    def test_logged_in_users_can_logout(self):
+        response = self.logout()
+        self.assertNotIn(b'Goodbye!', response.data)
+
+
+    def test_default_user_role(self):
+        db.session.add(
+            User(
+                "Johnny",
+                "john@doe.com",
+                "johnny"
+            )
+        )
+
+        db.session.commit()
+
+        users = db.session.query(User).all()
+        print(users)
+        for user in users:
+            self.assertEquals(user.role, "user")
+
+if __name__ == "__main__":
+    unittest.main()
--- project/views.py
+++ project/views.py
@@ -4,6 +4,7 @@ from functools import wraps
 from flask import Flask, flash, redirect, render_template, \
     request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 import datetime
 
 # config
@@ -26,11 +27,29 @@ def login_required(test):
     return wrap
 
 
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash("Error in the {} field - {}".format((getattr(form, field).label.text, error), 'error'))
+
+
+def open_tasks():
+    return db.session.query(Task).filter_by(
+        status='1').order_by(Task.due_date.asc())
+
+
+def closed_tasks():
+    return db.session.query(Task).filter_by(
+        status='0').order_by(Task.due_date.asc())
+
+
 # route handlers
 @app.route('/logout/')
+@login_required
 def logout():
     session.pop('logged_in', None)
     session.pop('user_id', None)
+    session.pop('role', None)
     flash('Goodbye!')
     return redirect(url_for('login'))
 
@@ -45,36 +64,31 @@ def login():
             if user is not None and user.password == request.form['password']:
                 session['logged_in'] = True
                 session['user_id'] = user.id
-                flash("Shake n' Bake!")
+                session['role'] = user.role
+                flash("Welcome!")
                 return redirect(url_for('tasks'))
             else:
-                error = f"I'm sorry {form.name.data or 'Dave'}. I can't do that."
-        else:
-            error = "Both fields are required..."
+                error = "Invalid username or password."
     return render_template('login.html', form=form, error=error)
 
 
 @app.route('/tasks/')
 @login_required
 def tasks():
-    open_tasks = db.session.query(Task) \
-        .filter_by(status='1').order_by(Task.due_date.asc())
-    closed_tasks = db.session.query(Task) \
-        .filter_by(status='0').order_by(Task.due_date.asc())
     return render_template(
         'tasks.html',
         form=AddTaskForm(request.form),
-        open_tasks=open_tasks,
-        closed_tasks=closed_tasks
+        open_tasks=open_tasks(),
+        closed_tasks=closed_tasks()
     )
 
 
 @app.route('/add/', methods=['GET', 'POST'])
 @login_required
 def new_task():
+    error = None
     form = AddTaskForm(request.form)
     if request.method == 'POST':
-        # import pdb; pdb.set_trace()
         if form.validate_on_submit():
             new_task = Task(
                 form.name.data,
@@ -88,30 +102,43 @@ def new_task():
             db.session.commit()
             flash('New entry was successfully posted!')
             return redirect(url_for('tasks'))
-        else:
-            flash('All fields are required.')
-            return redirect(url_for('tasks'))
-    return render_template('tasks.html', form=form)
+    return render_template(
+        'tasks.html',
+        form=form,
+        error=error,
+        open_tasks=open_tasks(),
+        closed_tasks=closed_tasks()
+    )
 
 
 @app.route('/complete/<int:task_id>/')
 @login_required
 def complete(task_id):
     new_id = task_id
-    db.session.query(Task).filter_by(task_id=new_id).update({"status": "0"})
-    db.session.commit()
-    flash('The task was marked as complete.')
-    return redirect(url_for('tasks'))
+    task = db.session.query(Task).filter_by(task_id=new_id)
+    if session['user_id'] == task.first().user_id or session['role'] == 'admin':
+        task.update({"status": "0"})
+        db.session.commit()
+        flash('The task is complete. Nice.')
+        return redirect(url_for('tasks'))
+    else:
+        flash('You can only update tasks that belong to you.')
+        return redirect(url_for('tasks'))
 
 
 @app.route('/delete/<int:task_id>/')
 @login_required
 def delete_entry(task_id):
     new_id = task_id
-    db.session.query(Task).filter_by(task_id=new_id).delete()
-    db.session.commit()
-    flash('The task was deleted.')
-    return redirect(url_for('tasks'))
+    task = db.session.query(Task).filter_by(task_id=new_id)
+    if session['user_id'] == task.first().user_id or session['role'] == 'admin':
+        task.delete()
+        db.session.commit()
+        flash('The task was deleted.')
+        return redirect(url_for('tasks'))
+    else:
+        flash('You can only delete tasks that belong to you.')
+        return redirect(url_for('tasks'))
 
 
 @app.route('/register/', methods=['GET', 'POST'])
@@ -125,8 +152,12 @@ def register():
                 form.email.data,
                 form.password.data
             )
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Thanks for joining!')
-            return redirect(url_for('login'))
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Thanks for registering. Please login.')
+                return redirect(url_for('login'))
+            except IntegrityError:
+                error = "That username and/or email already exists."
+                return render_template('register.html', form=form, error=error)
     return render_template('register.html', form=form, error=error)
