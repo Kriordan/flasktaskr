3a12feed66e8922195fa841952312c6795f72590
Keith Riordan <keith.riordan@gmail.com>
2018-02-23 16:04:46 -0500

    Reorganized app structure using Blueprint design pattern

---
+++ db_create.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+from project import db
+from project.models import Task, User
+
+
+# create the database and the db table
+db.create_all()
+
+# insert data
+db.session.add(
+    User('admin', 'ad@min.com', 'admin', 'admin')
+)
+db.session.add(
+    Task("Finish this tutorial", date(2018, 2, 14), 10, date(2018, 2, 11), 1, 1)
+)
+db.session.add(
+    Task("Finish Real Python", date(2018, 2, 14), 10, date(2018, 2, 11), 1, 1)
+)
+
+# commit the changes
+db.session.commit()
